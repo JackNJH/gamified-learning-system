54e18d46aec89d5679b8262868895e4fc194677a
Jack <ngjanhwan@gmail.com>
2024-03-09 01:01:25 +0800

    redundant code

--- modules/update_badge.php
+++ modules/update_badge.php
@@ -4,7 +4,6 @@ include '../modules/config.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['badgeID']) && isset($_POST['slot'])) {
     $badgeID = $_POST['badgeID'];
     $slot = $_POST['slot'];
-    $studentID = $student_id; // Assuming $student_id is defined in config.php
 
     // Update the selected badge slot for the student
     $sql = "UPDATE student SET SelectedBadge$slot = $badgeID WHERE StudentID = '$student_id'";
