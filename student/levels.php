b861fb58a3dbb155f473c52f6f984c82212c954b
Nat <148355575+nat-c69@users.noreply.github.com>
2024-03-17 13:04:26 +0800

    Update levels.php

--- student/levels.php
+++ student/levels.php
@@ -6,7 +6,18 @@
     }
 
     include '../components/header.php'; // Header
+    $data = isset($_GET['chapter_id']) ? $_GET['chapter_id'] : '';
+    
+    if ($data === '') {
+        // Handle case when user_id is not provided or invalid
+        die("User ID not provided or invalid.");
+    }
 
+    $sql = "SELECT * FROM `level` WHERE ChapterID= '$data'";
+    $result = mysqli_query($conn,$sql);
+
+    $sql1 = "SELECT * FROM chapter WHERE ChapterID= '$data'";
+    $result1 = mysqli_query($conn,$sql1);
 ?>
 
 <!DOCTYPE html>
@@ -30,37 +41,56 @@
         </div>
     </div>  
 
-    <div class="header">Chapter 1</div>
+    <?php while($row1=mysqli_fetch_assoc($result1))
+        {                    
+    ?>
+        
+        <div class="header"><?php echo $row1['ChapterName'];?></div>
+    <?php
+        }
+    ?>
+
     <div class="wrapper">
         <div class="levelselect">
             <div class="list">
-                <form>
-                    <input type="radio" id="lvl1" name="level-option" value="lvl1" checked />
-                    <label for="lvl1">Level 1</label>
-                    <br>
-                    <br>
-                    <input type="radio" id="lvl2" name="level-option" value="lvl2">
-                    <label for="lvl1">Level 2</label>
-                    <br>
-                    <br>
-                    <input type="radio" id="lvl3" name="level-option" value="lvl3">
-                    <label for="lvl1">Level 3</label>
+
+            <?php while($row=mysqli_fetch_assoc($result))
+                 {                    
+            ?>
+                <form method="POST">
+                    Level
+                    <input type="submit" value="<?php echo $row['LevelID'];?>" name="mybutton">
                 </form>
+            <?php
+                }
+            ?>
             </div>
         </div>
+
         
         <div class="guide">
-            <p>CONTENT</p>
-            <p>(Video, pics, guides etc)</p>
+            <?php
+            if(isset($_POST['mybutton'])){
+                $level = $_POST['mybutton'];
+                $sql2 = "SELECT * FROM `level` WHERE LevelID= '$level'";
+                $result2 = mysqli_query($conn,$sql2);
+                    while($row=mysqli_fetch_assoc($result2))
+                    {              
+            
+                        echo $row['LevelContent'];
+                    }
+                }
+            ?>
         </div>
 
-        <div class="back">
-            <a href="chapter.php">&#8592; Chapters</a>
-        </div>
+       
+        <button class ="back" onclick="history.back()">&#8592; Chapters</button>
+      
         <div class="next">
             <a href="questions.php">Questions &#8594;</a>
         </div>
-    </div>
 
+    </div>
+    
 </body>
 </html>
