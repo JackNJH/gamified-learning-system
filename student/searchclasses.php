ed5b3a1c579974e14e1748e6932e11bfeca850bf
Nat <148355575+nat-c69@users.noreply.github.com>
2024-03-17 13:04:54 +0800

    Update searchclasses.php

--- student/searchclasses.php
+++ student/searchclasses.php
@@ -7,10 +7,7 @@
 
     include '../components/header.php'; // Header
 
-    
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,17 +23,19 @@
         <div class="options">
            <a href="index.php"><b>Student menu</a></b>
         </div>
+        
         <div class="classes">
-            <?php
+
+        <?php
                 if (isset($_SESSION['recent_pages'])) {
                     echo "<h3>Recently Visited Pages:</h3>";
                     echo "<ul>";
                     foreach ($_SESSION['recent_pages'] as $page) {
-                    echo "<li><a href='$page'>$page</a></li>";
+                    echo "<li><a href='$page'>class</a></li>";
                     }
                     echo "</ul>";
-                } else {
-                    echo "<p><b>No recent pages available.</p></b>";
+                 } else {
+                    echo "<p>No recent pages available.</p>";
                 }
             ?>
         </div>
@@ -46,12 +45,13 @@
         <form method="post">
             <input type="text" placeholder=" Search for classes" name="search">
     
-            <button name="submit"></button>
+            <button name="submit">
+                <img src="../images/search.png">
+            </button>
         </form>
     </div>
     
     <div class="searchresult">
-
         <?php
             if(isset($_POST['submit'])){
                 $search=$_POST['search'];
@@ -63,30 +63,36 @@
 
                 if($result){
                     if(mysqli_num_rows($result)>0){
+
                         while($row=mysqli_fetch_assoc($result))
                         {
+                           
+                            
         ?>
-            <a href= "chapter.php" style="text-decoration:none">
-                <div class="box">
-                    <div class="image">
-                        <img src="<?php echo $row["ClassDashboard"]; ?>" alt="img">
-                    </div>
-                    <div class="difficulty"><?php echo $row["ClassDiff"];?></div>
-                    <div class="data">
-                        <div class="className"><b><?php echo $row["ClassName"];?></div></b>
-                        <div class="description"><?php echo $row["ClassDesc"];?></div>
-                    </div>
+            <form action="class.php" method="get">
                 
-                </div>
-            </a>
-            
+                    <?php echo "<a class='classlink' href='../student/class?class_id={$row['ClassID']}'>";?>
+                        <div class="box">
+                            <div class="image">
+                                <img src="<?php echo $row['ClassDashboard']; ?>" alt="img">
+                            </div>
+                            <div class="difficulty"><?php echo $row['ClassDiff'];?></div>
+                            <div class="data">
+                                <br>
+                                <div class="className"><b><?php echo $row['ClassName'];?></div></b>
+                                <div class="description"><?php echo $row['ClassDesc'];?></div>                                
+                            </div>    
+                        </div>    
+                    <?php "</a>";?>
+               
+            </form>
         <?php
                         }
                     }
                 }  
             }      
         ?>
-    </div>
+    </div> 
 </body>
 </html>
 
